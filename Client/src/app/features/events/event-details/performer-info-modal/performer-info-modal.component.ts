d1f80a2aa959e0e4f60f3b4503620b8b4ae0c067
Roman Kostukov <romank057yl@gmail.com>
2026-06-12 14:43:47 +0300

    UIFix

--- Client/src/app/features/events/event-details/performer-info-modal/performer-info-modal.component.ts
+++ Client/src/app/features/events/event-details/performer-info-modal/performer-info-modal.component.ts
@@ -1,19 +1,25 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Input, Output, OnInit, inject, signal, computed } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
 import { TranslateModule } from '@ngx-translate/core';
 import { PerformerDetails } from '../../dtos/performer-details.dto';
+import { EventService } from '../../../events/services/event.service';
 import { ASSET_CONSTANTS } from '../../../../core/constants/asset-constants';
 
 @Component({
   selector: 'app-performer-info-modal',
   standalone: true,
-  imports: [CommonModule, TranslateModule],
+  imports: [CommonModule, TranslateModule, RouterLink],
   templateUrl: './performer-info-modal.component.html',
-  styleUrls: ['./performer-info-modal.component.scss']
-})
-export class PerformerInfoModalComponent {
+  styleUrls: ['./performer-info-modal.component.scss']})
+export class PerformerInfoModalComponent implements OnInit {
   @Input({ required: true }) performer!: PerformerDetails;
   @Output() closeModal = new EventEmitter<void>();
+
+  private readonly eventService = inject(EventService);
+
+  public performerEvents = signal<any[]>([]);
+  public isLoadingEvents = signal<boolean>(true);
 
   public get avatarSrc(): string {
     return ASSET_CONSTANTS.getPerformerAvatar(this.performer.avatarUrl, this.performer.id);
@@ -38,6 +44,30 @@ export class PerformerInfoModalComponent {
     return this.performer.youtubeUrl.startsWith('http')
       ? this.performer.youtubeUrl
       : `https://youtube.com/${this.performer.youtubeUrl}`;
+  }
+
+  public ngOnInit(): void {
+    this.loadPerformerEvents();
+  }
+
+  private loadPerformerEvents(): void {
+    this.eventService.getApprovedEvents().subscribe({
+      next: (events) => {
+        const now = new Date();
+        const filtered = events.filter((e: any) => {
+          const hasPerformer = (e.performers || e.Performers || [])
+            .some((p: any) => p.id === this.performer.id);
+          const isFuture = new Date(e.startDate).getTime() > now.getTime();
+          return hasPerformer && isFuture;
+        });
+        
+        this.performerEvents.set(filtered);
+        this.isLoadingEvents.set(false);
+      },
+      error: () => {
+        this.isLoadingEvents.set(false);
+      }
+    });
   }
 
   public close(): void {
